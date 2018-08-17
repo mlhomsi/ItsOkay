2defb0f72fb109f882f46cd6069a142c4ede7f76
mlhomsi <mlhomsi@gmail.com>
2018-08-17 05:57:27 -0300

    Tudo

---
+++ Assets/Seta.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seta : MonoBehaviour {
+
+    public GameObject GotIt;
+    public string Orientation;
+    public Animator SetaBehaviour;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void NextArrow()
+    { Debug.Log("I Love You");
+        SetaBehaviour.SetBool("Done", true);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Orientation == "U")
+        {
+            if (Input.GetKeyDown("up"))
+            { GotIt.SetActive(true);
+                NextArrow();
+            }
+        }
+        if (Orientation == "D")
+        {
+            if (Input.GetKeyDown("down"))
+            { GotIt.SetActive(true);
+                NextArrow();
+            }
+        }
+        if (Orientation == "L")
+        {
+            if (Input.GetKeyDown("left"))
+            { GotIt.SetActive(true);
+                NextArrow();
+            }
+        }
+        if (Orientation == "R")
+        {
+            if (Input.GetKeyDown("right"))
+            { GotIt.SetActive(true);
+                NextArrow();
+            }
+        }
+
+
+    }
+}
